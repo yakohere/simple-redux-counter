7e02e74432aec7af2071b57185f96efe1b3adf87
devyako <yakhyoismoildjonv@gmail.com>
2020-03-25 13:50:46 +0500

    first commit

---
+++ redux-basics.js
@@ -0,0 +1,23 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0
+};
+
+const rootReducer = (state = initialState, action) => {
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
+  return state;
+};
+
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 15 });
+console.log(store.getState());
