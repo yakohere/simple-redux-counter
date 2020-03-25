148b8735467a40197d0b7693e5f5e5661bfdaf04
devyako <yakhyoismoildjonv@gmail.com>
2020-03-25 17:31:26 +0500

    setting react-redux

--- redux-basics.js
+++ redux-basics.js
@@ -5,6 +5,7 @@ const initialState = {
   counter: 0
 };
 
+//Reducer
 const rootReducer = (state = initialState, action) => {
   if (action.type === "ADD_COUNTER") {
     return {
@@ -15,9 +16,16 @@ const rootReducer = (state = initialState, action) => {
   return state;
 };
 
+//Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+//Subscribtion
+store.subscribe(() => {
+  console.log("[Subscribtion]", store.getState());
+});
+
+//Dispatching Action
 store.dispatch({ type: "INC_COUNTER" });
 store.dispatch({ type: "ADD_COUNTER", value: 15 });
 console.log(store.getState());
