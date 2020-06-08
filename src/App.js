73315da12bbd4f0c19ffebe75f0c5b2f563f27d8
devyako <yakhyoismoildjonov@gmail.com>
2020-06-08 21:05:01 +0500

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [count, setCount] = useState(1);
   return (
     <div className="App">
-      <Counter />
+      <p>{count}</p>
+      <button onClick={increaseHandler}>INCREMENT</button>
+      <button onClick={increaseHandler}>DECREMENT</button>
     </div>
   );
 };
