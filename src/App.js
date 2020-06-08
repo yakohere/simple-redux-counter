efe58ec6c0ff31cba51a828f38c3c834b3726586
devyako <yakhyoismoildjonov@gmail.com>
2020-06-08 21:12:59 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
 import "./App.css";
 
+const useCounter = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseHandler = () => {
+    setCount(count + 1);
+  };
+  const decreaseHandler = () => {
+    setCount(count - 1);
+  };
+  const resetHandler = () => {
+    setCount(0);
+  };
+
+  return { count, increaseHandler, decreaseHandler, resetHandler };
+};
+
 const App = () => {
-  const [count, setCount] = useState(1);
+  const {
+    count,
+    increaseHandler,
+    decreaseHandler,
+    resetHandler,
+  } = useCounter();
+
   return (
     <div className="App">
       <p>{count}</p>
       <button onClick={increaseHandler}>INCREMENT</button>
-      <button onClick={increaseHandler}>DECREMENT</button>
+      <button onClick={decreaseHandler}>DECREMENT</button>
+      <button onClick={resetHandler}>RESET</button>
     </div>
   );
 };
